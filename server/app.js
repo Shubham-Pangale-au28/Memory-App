033b1d69d80017b2be7d1927fb7eb28df7c90902
pangaleshubham10@gmail.com <pangaleshubham10@gmail.com>
2022-10-14 13:13:50 +0530

    Capston Project

---
+++ server/app.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+const postRoutes = require('./routes/posts.js');
+const userRouter = require("./routes/user.js");
+
+const app = express();
+
+app.use(bodyParser.json({ limit: '30mb', extended: true }))
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
+app.use(cors())
+
+app.use('/posts', postRoutes);
+app.use("/user", userRouter);
+
+app.get('/', (req, res)=>{
+    res.send("APP is Running......")
+})
+const CONNECTION_URL = 'mongodb+srv://shubham40:Pass%40123@cluster0.wlhsk.mongodb.net/?retryWrites=true&w=majority';
+// const CONNECTION_URL = 'mongodb://localhost:27017'
+const PORT = process.env.PORT|| 5000;
+
+mongoose
+    .connect(CONNECTION_URL)
+    .then(() => console.log("Database Connection Successfully!!..."))
+    .catch((err) => {
+        console.log(err);
+    });
+
+
+app.listen(PORT, () => {
+    console.log(`Server Started On Port http://localhost: ${PORT}`);
+});
