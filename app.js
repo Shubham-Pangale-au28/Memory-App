59d9a1f4c667e2e310af86a72d3deeb53950d77f
pangaleshubham10@gmail.com <pangaleshubham10@gmail.com>
2022-12-17 12:40:06 +0530

    New changes

--- app.js
+++ app.js
@@ -15,9 +15,6 @@ app.use(cors())
 app.use('/posts', postRoutes);
 app.use("/user", userRouter);
 
-app.get('/', (req, res)=>{
-    res.send("APP is Running......")
-})
 const CONNECTION_URL = 'mongodb+srv://shubham40:Pass%40123@cluster0.wlhsk.mongodb.net/?retryWrites=true&w=majority';
 // const CONNECTION_URL = 'mongodb://localhost:27017'
 const PORT = process.env.PORT|| 5000;
