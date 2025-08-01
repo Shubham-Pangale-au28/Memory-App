91fbbbd726c8d3aa6944273680d42ee4f5bd374a
shubhampangale <shubham.pangale@gmail.com>
2025-08-01 18:54:07 +0530

    New package added and update mongo url

--- app.js
+++ app.js
@@ -1,32 +1,31 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const mongoose = require('mongoose');
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const mongoose = require("mongoose");
 
-const postRoutes = require('./routes/posts.js');
+const postRoutes = require("./routes/posts.js");
 const userRouter = require("./routes/user.js");
 
 const app = express();
 
-app.use(bodyParser.json({ limit: '30mb', extended: true }))
-app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
-app.use(cors())
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
 
-app.use('/posts', postRoutes);
+app.use("/posts", postRoutes);
 app.use("/user", userRouter);
 
-const CONNECTION_URL = 'mongodb+srv://shubham40:Pass%40123@cluster0.wlhsk.mongodb.net/?retryWrites=true&w=majority';
-// const CONNECTION_URL = 'mongodb://localhost:27017'
-const PORT = process.env.PORT|| 5000;
+// const CONNECTION_URL = 'mongodb+srv://shubham40:Pass%40123@cluster0.wlhsk.mongodb.net/?retryWrites=true&w=majority';
+const CONNECTION_URL = "mongodb://admin:secret@192.168.2.244:27017/admin?authSource=admin";
+const PORT = process.env.PORT || 5000;
 
 mongoose
-    .connect(CONNECTION_URL)
-    .then(() => console.log("Database Connection Successfully!!..."))
-    .catch((err) => {
-        console.log(err);
-    });
-
+  .connect(CONNECTION_URL)
+  .then(() => console.log("Database Connection Successfully!!..."))
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.listen(PORT, () => {
-    console.log(`Server Started On Port http://localhost: ${PORT}`);
+  console.log(`Server Started On Port http://localhost: ${PORT}`);
 });
